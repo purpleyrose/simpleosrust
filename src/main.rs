2953b1da737f94fb0a0d4ae7057768a77fb4fc92
Pamela Rose <pam.is.cool123@gmail.com>
2023-06-11 01:38:48 -0400

    Added the ability to print to screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
+mod vga_buffer;
 
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
     loop{}
 }
 
